8bf8bb77512d144166a96044513eac9d5f9825ac
phattantran1997 <phattantran123@gmail.com>
2024-04-05 17:07:16 +1000

    add token for Instagram blogs in About section

--- src/assets/layout/components/About/About.jsx
+++ src/assets/layout/components/About/About.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { faBlog, faMusic, faPlane, faSpoon, faWeight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { Octokit } from "@octokit/rest";
+import axios from "axios";
 
 const highlightLocations = [
     {
@@ -26,6 +29,78 @@ const highlightLocations = [
         description: "Sa Pa, Lào Cai, Vietnam"
     },
     {
+        latlong: [15.5394, 108.0191], // Quảng Nam, Hội An
+        description: "Quảng Nam, Hội An, Vietnam"
+    },
+    {
+        latlong: [15.1214, 108.8044], // Quảng Ngãi
+        description: "Quảng Ngãi, Vietnam"
+    },
+    {
+        latlong: [13.8079, 108.1094], // Quảng Ngãi
+        description: "Gia Lai, Vietnam"
+    },
+    {
+        latlong: [14.3497, 108.0005], // Quảng Ngãi
+        description: "Kon Tum, Vietnam"
+    },
+    {
+        latlong: [13.947536, 109.322327], // Bình Định
+        description: "Bình Định, Vietnam"
+    },
+    {
+        latlong: [13.088186, 109.294844], // Phú Yên
+        description: "Phú Yên, Vietnam"
+    },
+    {
+        latlong: [11.940419, 108.458313], // Đà Lạt
+        description: "Đà Lạt, Vietnam"
+    },
+    {
+        latlong: [12.238791, 109.196748], // Nha Trang
+        description: "Nha Trang, Vietnam"
+    },
+    {
+        latlong: [10.933331, 108.100014], // Phan Thiết
+        description: "Phan Thiết, Vietnam"
+    },
+    {
+        latlong: [10.776889, 106.700859], // Hồ Chí Minh
+        description: "Hồ Chí Minh, Vietnam"
+    },
+    {
+        latlong: [11.607470, 108.864449], // Ninh Thuận
+        description: "Ninh Thuận, Vietnam"
+    },
+    {
+        latlong: [11.020780, 107.169968], // Đồng Nai
+        description: "Đồng Nai, Vietnam"
+    },
+    {
+        latlong: [11.541750, 106.883240], // Bình Phước
+        description: "Bình Phước, Vietnam"
+    },
+    {
+        latlong: [10.353000, 106.360977], // Tiền Giang
+        description: "Tiền Giang, Vietnam"
+    },
+    {
+        latlong: [9.948941, 106.345335], // Trà Vinh
+        description: "Trà Vinh, Vietnam"
+    },
+    {
+        latlong: [10.035870, 105.722236], // Kiên Giang
+        description: "Kiên Giang, Vietnam"
+    },
+    {
+        latlong: [10.664372, 106.299156], // Long An
+        description: "Long An, Vietnam"
+    },
+    {
+        latlong: [12.242460, 108.441933], // Đăk Lăk
+        description: "Đăk Lăk, Vietnam"
+    },
+    {
         latlong: [9.824959, 105.125893],
         description: "Kien Giang, Vietnam"
     },
@@ -42,24 +117,98 @@ const highlightLocations = [
         description: "Bangkok, Thailand"
     }
 ]
+
+
 const About = () => {
+    const [posts, setPosts] = useState([]);
+    const accessToken = import.meta.env.VITE_INSTAGRAM_ACCESS_TOKEN;
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption&access_token=${accessToken}`);
+                setPosts(response.data.data);
+            } catch (error) {
+                console.error('Error fetching posts:', error.message);
+            }
+        };
+
+        fetchPosts();
+    }, [accessToken]);
+    const [showFullCaption, setShowFullCaption] = useState(true);
+
+    const toggleCaption = () => {
+        setShowFullCaption(prev => !prev);
+    };
     return (
-        <div className="">
+        <div className="pt-20">
             <div className="container mx-auto">
                 {/* Introduction Section */}
                 <section className="mb-20">
-                    <h2 className="text-3xl font-bold mb-4">About Me</h2>
+                    <h2 className="title_header text-3xl font-bold mb-4">About Me</h2>
                     <p>
-                    <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> My name is <span className="font-bold">Lincoln Tran</span>, and I was born on <span className="font-bold">September 7th, 1997</span> in <span className="font-bold">Vietnam</span>, where I spent my formative years and received my education. From a young age, I developed a keen interest in technology and problem-solving, which eventually led me to pursue a career as a <span className="font-bold">Software Engineer</span>.
+                        <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> My name is <span className="font-bold">Lincoln Tran</span>, and I was born on <span className="font-bold">September 7th, 1997</span> in <span className="font-bold">Vietnam</span>, where I spent my formative years and received my education. From a young age, I developed a keen interest in technology and problem-solving, which eventually led me to pursue a career as a <span className="font-bold">Software Engineer</span>.
                     </p>
                     <p>
-                    <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> With more than <span className="font-bold">3 years of experience</span> designing, testing, and developing software solutions, I've honed my skills in delivering innovative business solutions. My <span className="font-bold">in-depth understanding of software architecture technologies</span> allows me to tackle complex challenges and provide effective solutions.
+                        <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> With more than <span className="font-bold">3 years of experience</span> designing, testing, and developing software solutions, I've honed my skills in delivering innovative business solutions. My <span className="font-bold">in-depth understanding of software architecture technologies</span> allows me to tackle complex challenges and provide effective solutions.
                     </p>
                     <p>
-                    <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> What sets me apart is my ability to thrive in <span className="font-bold">fast-paced, high-energy, and deadline-driven environments</span>. I'm a <span className="font-bold">meticulous</span> professional who takes pride in my work and is always willing to take on additional tasks to ensure project success. Collaboration and effective communication are key strengths of mine, enabling me to work seamlessly with cross-functional teams.
+                        <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> What sets me apart is my ability to thrive in <span className="font-bold">fast-paced, high-energy, and deadline-driven environments</span>. I'm a <span className="font-bold">meticulous</span> professional who takes pride in my work and is always willing to take on additional tasks to ensure project success. Collaboration and effective communication are key strengths of mine, enabling me to work seamlessly with cross-functional teams.
                     </p>
                 </section>
+                {/* Instagram blog Section */}
+                <section className="mb-20">
+                    <div className="title_header flex items-center">
+                        <h2 className="text-3xl font-bold mb-4">
+                            My Instagram Daily Blogs
+                            <FontAwesomeIcon icon={faInstagram} style={{ marginLeft: '8px' }} />
+                            <FontAwesomeIcon icon={faInstagram} style={{ marginLeft: '8px' }} />
+                            <FontAwesomeIcon icon={faInstagram} style={{ marginLeft: '8px' }} />
+                        </h2>
+                    </div>
+                    <div>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                            {posts.map(post => (
 
+                                <div key={post.id} className="bg-white rounded-lg shadow-md overflow-hidden">
+                                    <img src={post.media_url} alt={post.caption} className="w-full" />
+                                    <div className="p-4">
+                                        <p className="text-gray-700 text-sm mb-2">
+                                            Posted by: <span className="font-bold">{post.username}</span>
+                                        </p>
+                                        <p className="text-gray-700 text-sm mb-2">
+                                            Time: {new Date(post.timestamp).toLocaleString()}
+                                        </p>
+                                        <div>
+                                            {
+                                                post.caption.length > 100 ? (
+                                                    <div>
+                                                        {
+                                                            showFullCaption ? (
+                                                                <div><p className="text-gray-800">{`${post.caption.slice(0, 100)}...`}</p>
+                                                                <span className="text-blue-500 cursor-pointer" onClick={toggleCaption}>
+                                                                    Read more
+                                                                </span></div>
+                                                            ) : (<div>
+                                                                <p className="text-gray-800">{post.caption}</p>
+                                                                <span className="text-blue-500 cursor-pointer" onClick={toggleCaption}>
+                                                                    Show Less
+                                                                </span>
+                                                            </div>)
+                                                        }
+                                                    </div>
+
+                                                ) :
+                                                    (<p className="text-gray-800">{post.caption}</p>)
+
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </section>
                 {/* Travel Section */}
                 <section className="mb-20">
                     <div className="title_header flex items-center">
@@ -71,10 +220,10 @@ const About = () => {
                         </h2>
                     </div>
                     <p>
-                    <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> While my passion for software engineering drives me professionally, my love for travel and exploring new cultures is equally profound. From a young age, I've been fascinated by the world's diversity and have seized every opportunity to embark on incredible journeys.
+                        <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> While my passion for software engineering drives me professionally, my love for travel and exploring new cultures is equally profound. From a young age, I've been fascinated by the world's diversity and have seized every opportunity to embark on incredible journeys.
                     </p>
                     <p>
-                    <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> My travels have taken me to some of the most breathtaking destinations across the globe, each offering a unique perspective and enriching my life with unforgettable experiences. Join me as I reminisce about the places I've visited and the adventures I've embarked upon.
+                        <span className="mr-2 text-xl font-bold text-blue-500">&#8226;</span> My travels have taken me to some of the most breathtaking destinations across the globe, each offering a unique perspective and enriching my life with unforgettable experiences. Join me as I reminisce about the places I've visited and the adventures I've embarked upon.
                     </p>
                     <div style={{ height: '700px' }}>
                         {/* Travel Map Component */}
@@ -98,30 +247,17 @@ const About = () => {
                         </MapContainer>
                     </div>
                 </section>
+
                 <div className="title_header flex items-center">
-                        <h2 className="text-3xl font-bold mb-4">
-                            My Music Adventures
-                            <FontAwesomeIcon icon={faMusic} style={{ marginLeft: '8px' }} />
-                            <FontAwesomeIcon icon={faMusic} style={{ marginLeft: '8px' }} />
-                            <FontAwesomeIcon icon={faMusic} style={{ marginLeft: '8px' }} />
-                        </h2>
+                    <h2 className="text-3xl font-bold mb-4">
+                        My GYM Adventures
+                        <FontAwesomeIcon icon={faWeight} style={{ marginLeft: '8px' }} />
+                        <FontAwesomeIcon icon={faWeight} style={{ marginLeft: '8px' }} />
+                        <FontAwesomeIcon icon={faWeight} style={{ marginLeft: '8px' }} />
+                    </h2>
                 </div>
-                <div className="title_header flex items-center">
-                        <h2 className="text-3xl font-bold mb-4">
-                            My GYM Adventures
-                            <FontAwesomeIcon icon={faWeight} style={{ marginLeft: '8px' }} />
-                            <FontAwesomeIcon icon={faWeight} style={{ marginLeft: '8px' }} />
-                            <FontAwesomeIcon icon={faWeight} style={{ marginLeft: '8px' }} />
-                        </h2>
-                </div>
-                <div className="title_header flex items-center">
-                        <h2 className="text-3xl font-bold mb-4">
-                            My Blogs Adventures
-                            <FontAwesomeIcon icon={faBlog} style={{ marginLeft: '8px' }} />
-                            <FontAwesomeIcon icon={faBlog} style={{ marginLeft: '8px' }} />
-                            <FontAwesomeIcon icon={faBlog} style={{ marginLeft: '8px' }} />
-                        </h2>
-                </div>
+
+
             </div>
         </div>);
 };
--- src/assets/layout/components/Contact/Contact.jsx
+++ src/assets/layout/components/Contact/Contact.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 
-const FORM_ENDPOINT = "https://herotofu.com/start"; // TODO - update to the correct endpoint
-
 const ContactForm = () => {
   const [submitted, setSubmitted] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const inputs = e.target.elements;
@@ -17,90 +16,97 @@ const ContactForm = () => {
     // Integrate with Simple Email Service platform
     // Replace with your actual email service integration code
     sendEmail(data);
-
-    setSubmitted(true);
   };
 
   // Replace with your actual email sending function
   const sendEmail = (data) => {
     console.log("Sending email with data:", data);
     // Implement your email sending logic here
-  };
 
-  if (submitted) {
+    // After sending email, set submitted state to true
+    setSubmitted(true);
+  };
+  if (!submitted) {
     return (
-      <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
-      </>
+      <div style={{ paddingTop: "150px" }} className="container flex">
+        <div className="w-1/2 pr-4">
+          <img
+            style={{ width: "75%", height: "75%" }}
+            src="https://herotofu.com/images/question.min.svg"
+            alt="Contact"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="w-1/2 pl-4">
+          <div className="mb-4">
+            <h2 className="text-2xl font-bold">Get in Touch</h2>
+            <p>
+              <span className="inline-flex items-center">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Flag_of_Australia.svg/1200px-Flag_of_Australia.svg.png"
+                  alt="Australia Flag"
+                  className="w-7 h-5 mr-1"
+                />
+                Phone: +64 578575788
+              </span>
+            </p>
+            <p>My email: phattantran123@gmail.com</p>
+          </div>
+          <form onSubmit={handleSubmit} method="POST">
+            <div className="pt-0 mb-3">
+              <input
+                type="text"
+                placeholder="Your name"
+                name="name"
+                className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
+                required
+              />
+            </div>
+            <div className="pt-0 mb-3">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
+                required
+              />
+            </div>
+            <div className="pt-0 mb-3">
+              <textarea
+                placeholder="Your message"
+                name="message"
+                className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
+                required
+              />
+            </div>
+            <div className="pt-0 mb-3">
+              <button
+                className="active:bg-blue-600 hover:shadow-lg focus:outline-none px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 rounded shadow outline-none"
+                type="submit"
+              >
+                Send a message
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     );
+  } else {
+    return (
+      <div className="absolute inset-0 flex justify-center items-center">
+        <div className="text-center">
+          <div className="text-2xl">
+            Thank you!
+          </div>
+          <div className="text-md">
+            We'll be in touch soon.
+          </div>
+        </div>
+      </div>
+
+    )
   }
 
-  return (
- 
-    <div className="pt-20 container flex">
-      <div className="w-1/2 pr-4">
-        <img
-          style={{ 'width': '75%', 'height': '75%' }}
-          src="https://herotofu.com/images/question.min.svg"
-          alt="Contact"
-          className="w-full h-full"
-        />
-      </div>
-      <div className="w-1/2 pl-4">
-        <div className="mb-4">
-          <h2 className="text-2xl font-bold">Get in Touch</h2>
-          <p>
-            <span className="inline-flex items-center">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Flag_of_Australia.svg/1200px-Flag_of_Australia.svg.png"
-                alt="Australia Flag"
-                className="w-7 h-5 mr-1"
-              />
-              Phone: +64 578575788
-            </span>
-          </p>
-          <p>My email: phattantran123@gmail.com</p>
-        </div>
-        <form onSubmit={handleSubmit} method="POST">
-          <div className="pt-0 mb-3">
-            <input
-              type="text"
-              placeholder="Your name"
-              name="name"
-              className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-              required
-            />
-          </div>
-          <div className="pt-0 mb-3">
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-              required
-            />
-          </div>
-          <div className="pt-0 mb-3">
-            <textarea
-              placeholder="Your message"
-              name="message"
-              className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-              required
-            />
-          </div>
-          <div className="pt-0 mb-3">
-            <button
-              className="active:bg-blue-600 hover:shadow-lg focus:outline-none px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 rounded shadow outline-none"
-              type="submit"
-            >
-              Send a message
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
 };
 
 export default ContactForm;
